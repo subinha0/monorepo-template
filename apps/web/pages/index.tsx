f7475136415d61e498707d956946528260895dd1
subinha0 <sb.ha@fastfive.co.kr>
2025-02-28 09:53:17 +0900

    chore: some work

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,3 +1,9 @@
+import { cn } from '@repo/ui/utils/cn';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={cn('flex flex-col items-center justify-center h-screen')}>
+      <h1 className={cn('text-4xl font-bold')}>Hello World</h1>
+    </div>
+  );
 }
